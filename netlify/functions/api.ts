c8c5e8094997d84e8057c1804ba339442266a8ed
adpmcppro <contacto@adpmcp.org>
2026-04-11 12:15:34 +0100

    version 1.9.1

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,7 +1,12 @@
 import serverless from "serverless-http";
 import { app, createServer } from "../../server";
 
-// Initialize the server routes
-await createServer();
+let handlerInstance: any;
 
-export const handler = serverless(app);
+export const handler: any = async (event: any, context: any) => {
+  if (!handlerInstance) {
+    await createServer();
+    handlerInstance = serverless(app);
+  }
+  return handlerInstance(event, context);
+};
